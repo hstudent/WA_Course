ab499a30fb1cbdf49b7cf7a67498d2176da1558a
hstudent <humpt_tj@mail.ru>
2016-05-01 21:44:11 +0300

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,8 @@
+sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
+Тогда при решении очередного задания необходимые действия будут сведены к минимуму:
+
+git clone https://github.com/your_account/stepic_web_project.git /home/box/web
+bash /home/box/web/init.sh
