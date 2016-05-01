f710cad07c2a53255201bb1d8224f9973da5e931
hstudent <humpt_tj@mail.ru>
2016-05-01 12:02:32 +0100

    Update echo_server_multy.py

--- echo_server_multy.py
+++ echo_server_multy.py
@@ -9,15 +9,18 @@ def es(conn):
             data = conn.recv(1024)
             if not data:
                     break
-            sd = data.decode("utf-8") 
-            if sd == 'close':
+            sd = data.decode("utf-8")
+            print("raw:", data, "str:", sd)
+            if sd == 'close\r\n':
                     break;
             conn.send(data)
     conn.close()
-    
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('0.0.0.0', 2224))
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(('0.0.0.0', 2222))
 s.listen(10)
 while True:
     conn, addr = s.accept()
     es(conn)
+
